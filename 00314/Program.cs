545a8198c81dc496670b483b62c2b7f6bade9e53
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:39:09 +0800

    2

--- 00314/Program.cs
+++ 00314/Program.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 
-namespace _20220307
+namespace B10956035
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double meal_cost = 100;
-            int tip_percent = 15;
-            int tax_percent = 8;
-            int a = (int)meal_cost;
-            int b = a + tax_percent + tip_percent;
-            Console.WriteLine(b);
+            int num;
+            Console.WriteLine("輸入數字");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 != 0)
+            {
+                Console.WriteLine("odd Weird");
+            }
+            else if (num < 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (num <= 20)
+            {
+                Console.WriteLine("even Weird");
+            }
+            else
+            {
+                Console.WriteLine("over Weird");
+            }
         }
     }
 }
