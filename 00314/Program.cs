801d7e836a58c1c4a9458785821f8707232c99ec
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:39:09 +0800

    3 2

--- 00314/Program.cs
+++ 00314/Program.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 
-namespace _20220307
+namespace B10956035
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double meal_cost = 100;
-            int tip_percent = 15;
-            int tax_percent = 8;
-            int a = (int)meal_cost;
-            int b = a + tax_percent + tip_percent;
-            Console.WriteLine(b);
+            int num;
+            Console.WriteLine("輸入數字");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+            for (int k = 1; k <= num; k++)
+            {
+                Console.WriteLine("n*" + k);
+            }
+            Console.WriteLine("");
+            for (int a = 1; a <= 9; a++)
+            {
+                for (int b = 1; b <= 9; b++)
+                {
+                    Console.Write(a + "*" + b + "=" + a * b + " ");
+                }
+                Console.WriteLine(" ");
+            }
         }
     }
 }
